d0baade356668b3208b725a9cdd625cc365ceda4
shauryakaushal <shauryakaushal47@gmail.com>
2019-03-13 10:59:41 +0530

    changes

--- src/CircularLinkedList.java
+++ src/CircularLinkedList.java
@@ -1,9 +1,34 @@
 public class CircularLinkedList {
+       int size = 0;
+        Node head = null;
+        Node tail = null;
+        class Node{
+                int data;
+                Node next;
+                public Node(int data){
+                       this.data = data;
+                }
+                public Node(int data,Node next){
+                        this.data = data;
+                        this.next = next;
+                }
+                public int getData(){
+                        return data;
+                }
+                public void setData(int data){
+                        this.data = data;
+                }
+                public Node getnext(){
+                        return next;
+                }
+                public void setNext(Node next){
+                        this.next = next;
+
 }
-punlic void print(){
+public void print(){
     Node temp = head;
     do{
-        System.out.println(" " + temp.data);
+        System.out.println(" List Is Empty "); *//+ temp.data/*);
         temp=temp.next;
         }
     while(temp != head)
@@ -12,15 +37,27 @@ punlic void print(){
 
         public void addNodeToNodeHead(int data){
     Node n = new Node(data);
-    if (size == 0){
-        head = n;
+    if (size == 0)
+    { head = n;
         tail = n;
-        n.next = head;
+        n.next = head;}
         else
-            Node temp = head;
+    { Node temp = head;
         n.next = temp;
         head = n;
         tail.next = head;
         }
+    size++;
         }
+        public void addNodeToTail(int data){
+        if(size == 0){
+                addNodeToHead(data);
+        }
+        else{
+                Node node = new Node(data);
+                tail.next = node;
+        }
+        }
+
+
 
